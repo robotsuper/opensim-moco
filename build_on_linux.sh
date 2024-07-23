7fdac2ec5839dc6aa244ec2f95daec53faeb6940
dengbinfeng <dengbf>
2024-07-23 14:05:32 +0800

    修改编译过程

--- build_on_linux.sh
+++ build_on_linux.sh
@@ -1,12 +1,17 @@
 #/bin/bash
-git submodule update --init
+
+# 先配置 ssh-keygen ，到github，https://github.com/settings/ssh/new，因为里面改成了 git ssh
+# git submodule update --init
+
 mkdir ../moco_dependencies_build
 cd ../moco_dependencies_build
 cmake ../opensim-moco/dependencies
-make --jobs 4 ipopt
-make --jobs 4
-cd ..
+make --jobs 16 ipopt
+make --jobs 16
+cd ../opensim-moco 
 mkdir build
 cd build
-cmake ../opensim-moco 
-make --jobs 4
+cmake .. -DCMAKE_INSTALL_PREFIX=$(pwd)/_install_
+make --jobs 16
+make install
+cd ..
