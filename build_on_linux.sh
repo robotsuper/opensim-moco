92b48d67397f4937929351e30e346db89964f33c
dengbinfeng <dengbf>
2024-07-25 20:32:27 +0800

    修改编译

--- build_on_linux.sh
+++ build_on_linux.sh
@@ -1,17 +1,21 @@
 #/bin/bash
 
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
 # 先配置 ssh-keygen ，到github，https://github.com/settings/ssh/new，因为里面改成了 git ssh
 # git submodule update --init
 
 mkdir ../moco_dependencies_build
 cd ../moco_dependencies_build
 cmake ../opensim-moco/dependencies
-make --jobs 16 ipopt
-make --jobs 16
+make -j16 ipopt
+make -j16
+
 cd ../opensim-moco 
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$(pwd)/_install_
-make --jobs 16
+make -j32
 make install
+
 cd ..
